09a35aecb5b8f03468e7a1df23fb8f30b9355801
AllyssonEstadulho92 <163911342+AllyssonEstadulho92@users.noreply.github.com>
2026-08-24 01:31:01 +0100

    chore: identificar backups como V4.5

--- js/db.js
+++ js/db.js
@@ -97,7 +97,7 @@
     ]);
     return {
       schemaVersion: 4,
-      appVersion: '4.2.0',
+      appVersion: '4.5.0',
       exportedAt: new Date().toISOString(),
       records,
       activities,
